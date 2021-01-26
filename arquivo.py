9acd29fb23d13cbfb9f1fffe9fce2444add2297e
Mariana Medeiros <mariana.medeiros@rankmyapp.com.br>
2021-01-26 12:50:05 -0300

    Adiciona funcionalidade de tamanho de variavel e deixa nome da funcao mais descritivo

--- arquivo.py
+++ arquivo.py
@@ -1,3 +1,10 @@
-def myfunc(x):
+def variable_type(x):
     variable_type = type(x)
     print (f"Essa variável é do tipo {variable_type}")
+
+def variable_len(x):
+    try:
+        variable_len = len(x)
+        print(f"O tamanho dessa variável é {variable_len}")
+    except TypeError as e:
+        print(e)
